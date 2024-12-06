06a13808f418b0f67a0ee0ca9f249951c20488ec
Stefano Bianda <stefanobianda@ik.me>
2024-12-06 16:37:31 +0100

    header ready but link still not implemented

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,34 @@
-<div>
-    <h1>Header</h1>
-</div>
+<header class="bg-blue-600 text-black p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <div>
+            <h1 class="text-3xl font-semibold">
+                <a href="{{url('/')}}">SBSoft</a>
+            </h1>
+            <h3>By Stefano Bianda</h3>
+        </div>
+        <nav class="hidden md:flex items-center space-x-4">
+            <x-nav-link url='/projects' :active="request()->is('projects')">Projects</x-nav-link>
+            <x-nav-link url='/experiences' :active="request()->is('experiences')">Experiences</x-nav-link>
+            @auth
+            <x-logout-button />
+            @else
+            <x-nav-link url='/login' :active="request()->is('login')" icon="user">Login</x-nav-link>
+            @endauth
+        </nav>
+        <button id="hamburger" class="text-white md:hidden flex items-center">
+            <i class="fa fa-bars text-2xl"></i>
+        </button>
+    </div>
+    <!-- Mobile Menu -->
+    <nav
+        id="mobile-menu"
+        class="hidden md:hidden bg-blue-600 text-white mt-5 pb-4 space-y-2">
+        <x-nav-link url='/projects' :active="request()->is('projects')" :mobile="true">Projects</x-nav-link>
+        <x-nav-link url='/experiences' :active="request()->is('experiences')" :mobile="true">Experiences</x-nav-link>
+        @auth
+        <x-logout-button />
+        @else
+        <x-nav-link url='/login' :active="request()->is('login')" icon="user">Login</x-nav-link>
+        @endauth
+    </nav>
+</header>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{asset('css/style.css')}}"
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$title ?? 'SBSoft'}}</title>
 </head>
@@ -12,5 +14,6 @@
     <main class="container mx-auto p-4 mt-4">
         {{$slot}}
     </main>
+    <script src="{{asset('js/script.js')}}"></script>
 </body>
 </html>
