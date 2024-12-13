2c4ca66e8802f3302967deaba37e5c87604abd8c
Stefano Bianda <stefanobianda@ik.me>
2024-12-13 21:34:32 +0100

    seed the projects

---
+++ database/seeders/data/projects.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        "name" => "CICD Pipeline",
+        "shortDescription" => "Complete shared CICD pipeline used by all projects",
+        "role" => "Product Owner",
+        "description" => null,
+        "company" => "Viseca Payments SA",
+        "image" => null,
+    ],
+];
