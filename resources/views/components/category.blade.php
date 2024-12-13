d99167eea2ae85a2bea3379a5b1ffaf9185a9484
Stefano Bianda <stefanobianda@ik.me>
2024-12-13 15:32:51 +0100

    delete button is now a component

--- resources/views/components/category.blade.php
+++ resources/views/components/category.blade.php
@@ -10,13 +10,9 @@
                     <p class="my-4">{{$category->description}}</p>
                 </div>
             </a>
-            <form method="POST" action="{{route('categories.destroy', $category->id)}}" class="mt-10">
-                @csrf
-                @method('DELETE')
-                <button class="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-                    <i class="fas fa-trash mr-3"></i> Remove Category
-                </button>
-            </form>
+
+            <x-delete-button action="{{route('categories.destroy', $category->id)}}" text="Remove Category" />
+
             </div>
         </div>    
     @else
--- resources/views/components/skill.blade.php
+++ resources/views/components/skill.blade.php
@@ -7,14 +7,10 @@
                 <img src="{{ $skill->image ? asset('storage/' . $skill->image) : asset('images/no-image-available.png') }}" alt="no image available" class="h-20 m-1">
                 <h1 class="text-xl font-bold text-center">{{$skill->name}}</h1>
                 <p class="text-center">{{$skill->description}}</p>
-                <form method="POST" action="{{route('skills.destroy', $skill->id)}}" class="mt-10">
-                    @csrf
-                    @method('DELETE')
-                    <button class="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-                        <i class="fas fa-trash mr-3"></i> Remove Skill
-                    </button>
-                </form>
-            </div>
+
+                <x-delete-button action="{{route('skills.destroy', $skill->id)}}" text="Remove Skill" />
+
+                </div>
         </a>
     @else
         <a href="{{route('skills.edit', $skill->id)}}">
