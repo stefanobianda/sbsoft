24cc0ac6d24aff5175ee09412c936f236df535ad
Stefano Bianda <stefanobianda@ik.me>
2024-12-17 08:08:12 +0100

    button just under the items

--- resources/views/projects/skills.blade.php
+++ resources/views/projects/skills.blade.php
@@ -3,14 +3,18 @@
     <x-project :project=$project />
     <div class="bg-gray-200 p-4 my-4 flex grid grid-cols-1 md:grid-cols-4 gap-4 m-4 rounded-lg">
         @foreach ($project->linkedBySkills as $skill)
+        <div>
             <x-skill :skill=$skill />
             <x-form-button action="{{route('projects.skills.remove', [ 'project' => $project->id, 'skill' => $skill->id ])}}" :delete='true' icon='fa-unlink' text="Unlink Skill" />
+        </div>
         @endforeach
     </div>
     <div class="bg-gray-200 p-4 my-4 flex grid grid-cols-1 md:grid-cols-4 gap-4 m-4 rounded-lg">
         @foreach ($skills as $skill)
+        <div>
             <x-skill :skill=$skill />
             <x-form-button action="{{route('projects.skills.add', [ 'project' => $project->id, 'skill' => $skill->id ])}}" text="Link Skill" />
+        </div>
         @endforeach
     </div>
 
