37d8366f8044bbcfef22d784db87dcfc21203123
Stefano Bianda <stefanobianda@ik.me>
2024-12-17 12:05:12 +0100

    create component and add to main display

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,13 @@
 <body>
     <x-header />
     <main class="container mx-auto p-4 mt-4">
+        {{-- Display alert messages --}}
+        @if(session('success'))
+        <x-alert type="success" message="{{session('success')}}" />
+        @endif
+        @if(session('error'))
+        <x-alert type="error" message="{{session('error')}}" />
+        @endif
         {{$slot}}
     </main>
     <script src="{{asset('js/script.js')}}"></script>
