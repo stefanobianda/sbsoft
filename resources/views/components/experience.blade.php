21ed32c60b951ee9728f24ef96b2cb87d6b665de
Stefano Bianda <stefanobianda@ik.me>
2025-06-26 18:58:13 +0200

    make some few movements on the webpage

--- resources/views/components/experience.blade.php
+++ resources/views/components/experience.blade.php
@@ -1,6 +1,6 @@
 @props(['experience', 'background' => null])
 
-<div class="{{ $background }} items-center text-center rounded-lg flex flex-col justify-center text-left p-4 mb-4">
+<div class="{{ $background }} items-center text-center rounded-lg flex flex-col justify-center text-left p-4 mb-4 transform transition-transform duration-200 ease-in-out hover:scale-105 shadow-md hover:shadow-lg">
     <a href="{{route('experiences.show', $experience->id)}}">
         <h1 class="text-3xl font-bold mb-4">{{$experience->name}}</h1>
         <div class="flex justify-center mb-4">
--- resources/views/components/skill.blade.php
+++ resources/views/components/skill.blade.php
@@ -4,7 +4,7 @@
     @switch($list)
         @case('show')
             <a href="{{route('skills.show', $skill->id)}}">
-                <div class="{{ $background }} items-center rounded-lg flex flex-col justify-center text-center gap-4 m-4">
+                <div class="{{ $background }} items-center rounded-lg flex flex-col justify-center text-center gap-4 m-4 transform transition-transform duration-200 ease-in-out hover:scale-105 shadow-md hover:shadow-lg">
                     <img src="{{ $skill->image ? asset('storage/' . $skill->image) : asset('images/no-image-available.png') }}" alt="no image available" class="h-20 m-1">
                     <h1 class="text-xl font-bold text-center">{{$skill->name}}</h1>
                     @if ($text == 'yes')
@@ -15,7 +15,7 @@
             @break
         @case('edit')
             <a href="{{route('skills.edit', $skill->id)}}">
-                <div class="{{ $background }} items-center rounded-lg flex flex-col justify-center text-center gap-4 m-4">
+                <div class="{{ $background }} items-center rounded-lg flex flex-col justify-center text-center gap-4 m-4 transform transition-transform duration-200 ease-in-out hover:scale-105 shadow-md hover:shadow-lg">
                     <img src="{{ $skill->image ? asset('storage/' . $skill->image) : asset('images/no-image-available.png') }}" alt="no image available" class="h-20 m-1">
                     <h1 class="text-xl font-bold text-center">{{$skill->name}}</h1>
                     @if ($text == 'yes')
@@ -25,7 +25,7 @@
             </a>
             @break
         @default
-            <div class="{{ $background }} items-center rounded-lg flex flex-col justify-center text-center gap-4 m-4">
+            <div class="{{ $background }} items-center rounded-lg flex flex-col justify-center text-center gap-4 m-4 transform transition-transform duration-200 ease-in-out hover:scale-105 shadow-md hover:shadow-lg">
                 <img src="{{ $skill->image ? asset('storage/' . $skill->image) : asset('images/no-image-available.png') }}" alt="no image available" class="h-20 m-1">
                 <h1 class="text-xl font-bold text-center">{{$skill->name}}</h1>
                 @if ($text == 'yes')
@@ -35,7 +35,7 @@
     @endswitch
 @else
 <a href="{{route('skills.show', $skill->id)}}">
-    <div class="{{ $background }} items-center rounded-lg flex flex-col justify-center text-center gap-4 m-4">
+    <div class="{{ $background }} items-center rounded-lg flex flex-col justify-center text-center gap-4 m-4 transform transition-transform duration-200 ease-in-out hover:scale-105 shadow-md hover:shadow-lg">
         <img src="{{ $skill->image ? asset('storage/' . $skill->image) : asset('images/no-image-available.png') }}" alt="no image available" class="h-20 m-1">
         <h1 class="text-xl font-bold text-center">{{$skill->name}}</h1>
         @if ($text == 'yes')
